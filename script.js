9e7b32796e8395b9e0ff3cb75e92aa5b8ee94a05
Anup Kumar Mishra <anupkrmi11122006@gmail.com>
2026-05-03 16:48:24 +0530

    Added logic for nav bar and footer

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function() {
                const myHeaders = new Headers();
                myHeaders.append("Content-Type", "application/json");
 
-               // query payload (fixed totalSubmissionNum missing error)
+               // query payload
                const graphql = JSON.stringify({
                     query: "\n    query userSessionProgress($username: String!) {\n allQuestionsCount {\n difficulty\n count\n }\n matchedUser(username: $username) {\n username\n submitStats {\n acSubmissionNum {\n difficulty\n count\n submissions\n }\n totalSubmissionNum {\n difficulty\n count\n submissions\n }\n }\n }\n }\n    ",
                     variables: {"username": `${username}`}
