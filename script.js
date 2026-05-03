5d459b20a6a9a655c99f8cddc74873f69cb4d27d
Anup Kumar Mishra <anupkrmi11122006@gmail.com>
2026-05-03 16:44:50 +0530

    formatted the code and added comments

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+     // grabbing all the needed elements
      const searchButton = document.getElementById("search-btn");
-     const usernamoeInput = document.getElementById("username-input");
-     const statsContainer = document.querySelector("stats-container");
+     const usernameInput = document.getElementById("username-input");
+     const statsContainer = document.querySelector(".stats-container"); 
      const easyProgressCircle = document.querySelector(".easy-progress");
      const mediumProgressCircle = document.querySelector(".medium-progress");
      const hardProgressCircle = document.querySelector(".hard-progress");
-     const easyLabel = document.getElementById(".easy-label");
-     const mediumLabel = document.getElementById(".medium-label");
-     const hardLabel = document.getElementById(".hard-label");
-     const cardStatsConatainer = document.querySelector(".stats-cards")
+     const easyLabel = document.getElementById("easy-label"); 
+     const mediumLabel = document.getElementById("medium-label"); 
+     const hardLabel = document.getElementById("hard-label"); 
+     const cardStatsConatainer = document.querySelector(".stats-cards");
 
+     // basic check to ensure user didn't enter gibberish
      function validateUsername(username) {
           if(username.trim() === "") {
                alert("Please enter a valid username.");
                return false;
           }
-          const regex = /^[a-zA-Z0-9_-]{1,15}/;
+          const regex = /^[a-zA-Z0-9_-]{1,15}$/;
           const isMatching = regex.test(username);
           if(!isMatching) {
                alert("Username can only contain letters, numbers, underscores, and hyphens, and must be between 1 and 15 characters long.");
@@ -24,8 +26,8 @@ document.addEventListener("DOMContentLoaded", function() {
           return isMatching;
      }
 
+     // fetch the actual data
      async function fetchUserDetails(username) {
-
           try{
                searchButton.textContent = "Searching...";
                searchButton.disabled = true;
@@ -36,10 +38,12 @@ document.addEventListener("DOMContentLoaded", function() {
                const myHeaders = new Headers();
                myHeaders.append("Content-Type", "application/json");
 
+               // query payload (fixed totalSubmissionNum missing error)
                const graphql = JSON.stringify({
-                    query: "\n    query userSessionProgress($username: STring!) {\n allQuestionsCount {\n difficualty\n count\n }\n matchedUser(username: $username) {\n username\n submitStats {\n acSubmissionNum {\n difficulty\n count\n submissions\n }\n }\n }\n }\n    ",
+                    query: "\n    query userSessionProgress($username: String!) {\n allQuestionsCount {\n difficulty\n count\n }\n matchedUser(username: $username) {\n username\n submitStats {\n acSubmissionNum {\n difficulty\n count\n submissions\n }\n totalSubmissionNum {\n difficulty\n count\n submissions\n }\n }\n }\n }\n    ",
                     variables: {"username": `${username}`}
                })
+               
                const requestOptions = {
                     method: "POST",
                     headers: myHeaders,
@@ -58,7 +62,8 @@ document.addEventListener("DOMContentLoaded", function() {
                displayUserData(parsedData);
           }
           catch(error) {
-               statsContainer.innerHTML = "<p>${error.message}</p>";
+               statsContainer.innerHTML = `<p>${error.message}</p>`;
+               statsContainer.style.setProperty("display", "block");
           }
           finally{
                searchButton.textContent = "Search";
@@ -66,19 +71,21 @@ document.addEventListener("DOMContentLoaded", function() {
           }
      }
 
+     // handle the css circle updates
      function updateProgress(solved, total, label, circle){
-          const progressDegree = (solved / total) * 100;
-          circle.style.setProperty("--progree-degree", `${progressDegree}%`);
+          const progressDegree = total > 0 ? (solved / total) * 100 : 0;
+          circle.style.setProperty("--progress-degree", `${progressDegree}%`);
           label.textContent = `${solved}/${total}`;
      }
 
+     // display everything on the DOM
      function displayUserData(parsedData) {
-          const totalQues = parsedData.data.allQuestionCount[0].count;
-          const totalEasyQues = parsedData.data.allQuestionCount[1].count;
-          const totalMediumQues = parsedData.data.allQuestionCount[2].count;
-          const totalHardQues = parsedData.data.allQuestionCount[3].count;
+          statsContainer.style.setProperty("display", "block");
 
-          const solvedTotalQues = parsedData.data.matchedUser.submitStats.acSubmissionNum[0].count;
+          const totalEasyQues = parsedData.data.allQuestionsCount[1].count;
+          const totalMediumQues = parsedData.data.allQuestionsCount[2].count;
+          const totalHardQues = parsedData.data.allQuestionsCount[3].count;
+
           const solvedTotalEasyQues = parsedData.data.matchedUser.submitStats.acSubmissionNum[1].count;
           const solvedTotalMediumQues = parsedData.data.matchedUser.submitStats.acSubmissionNum[2].count;
           const solvedTotalHardQues = parsedData.data.matchedUser.submitStats.acSubmissionNum[3].count;
@@ -96,9 +103,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
           console.log("Card data: ", cardData);
 
+          // render out the bottom cards
           cardStatsConatainer.innerHTML = cardData.map(
                data => 
-                    `<div class="stats-card">
+                    `<div class="card">
                          <h4>${data.label}</h4>
                          <p>${data.value}</p>
                     </div>`
